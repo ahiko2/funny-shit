b9935908dfcba4356fab63cc56eb8e7c7b69e7cd
Ellli <51936143+ahiko2@users.noreply.github.com>
2020-07-14 01:59:22 +0900

    Update operatoroverloading2.cpp

--- operatoroverloading2.cpp
+++ operatoroverloading2.cpp
@@ -7,32 +7,31 @@ class rational
     private:
     int p,q;
     public:
-    rational( int p=0,int q=0)
+    rational(int p=0,int q=0)
     {
         this->p=p;
         this->q=q;
     }
     rational operator+(rational r)
     {
-        rational temp(0,0);
-        temp.p=p*r.p + q*r.p;
-        temp.q=q*r.q;
+        rational temp;
+        temp.p=p*r.q + q*r.p;
+        temp.q=r.q*q;
         return temp;
     }
-    friend ostream & operator<<(ostream &o,rational &r1);
-    
+    friend ostream & operator<<(ostream & out, rational c1); //printing function
 
 };
-ostream & operator<<(ostream &o,rational &r1)
+ostream & operator<<(ostream & out, rational c1)
 {
-    o<<r1.p<<" / " <<r1.q;
-    return o;
+    out<<c1.p <<"/"<<c1.q;
+    return out;
 }
 int main()
 {
-    rational r1(3,4),r2(4,5),r3;
+    rational r1(3,4),r2(2,5),r3;
     r3=r1+r2;
     cout<<r1<<" + "<<r2 <<" = ";
     cout<<r3<<endl;
-    
+
 }
