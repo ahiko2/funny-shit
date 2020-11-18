5c896a2988e5b11b90c3533ec5ac588e7ff10715
Ellli <51936143+ahiko2@users.noreply.github.com>
2020-11-19 02:57:58 +0900

    Create exceptionhandling.cpp

---
+++ exceptionhandling.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <exception>
+using namespace std;
+
+class overspeed:public exception{
+    private:
+    int speed;
+    public:
+    void setspeed(int speed)
+    {
+        this->speed=speed;
+    }
+    void getspeed(int speed)
+    {
+        cout<<"current speed is "<<speed<<endl;
+    }
+    const char * what()
+    {
+        return "check ur speed AGAIN";
+    }
+};
+
+class Car{
+    private:
+    int speed;
+    public:
+    Car(){
+        speed=0;
+        cout<<"speed is"<<speed<<endl;
+    }
+    void accerelate(){
+        for(;;){
+            speed+=10;
+            cout<<"speed is "<<speed<<endl;
+            if(speed>=100){
+                overspeed s;
+                s.setspeed(speed);
+                throw s;
+            }
+        }
+    }
+};
+
+int main(){
+    Car annia;
+    try{
+        annia.accerelate();
+    }
+    catch(overspeed s){
+        cout<<s.what()<<endl;
+    }
+}
