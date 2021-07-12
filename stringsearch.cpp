94e92441130c141622b292cff4da9f14752a5ba4
Ellli <51936143+ahiko2@users.noreply.github.com>
2021-07-12 21:42:30 +0900

    Create stringsearch.cpp

---
+++ stringsearch.cpp
@@ -0,0 +1,14 @@
+vector<int> stringSearch(string big,string small){
+  
+    vector<int> result = {};
+    int index=big.find(small);
+    while(index!=-1){
+        result.push_back(index);
+        index=big.find(small,index+1);
+    }
+   
+   return result;
+
+
+}
+//find the "small" position from "big" and save its index in "result"
